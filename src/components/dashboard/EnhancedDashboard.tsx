5000b88085fd9aa93b334e8bf1e9e78b860a2fd8
marvinmckinneyii0 <90293966+marvinmckinneyii0@users.noreply.github.com>
2025-09-13 15:47:36 +0100

    Remove duplicate DashboardFilters import

--- src/components/dashboard/EnhancedDashboard.tsx
+++ src/components/dashboard/EnhancedDashboard.tsx
@@ -39,7 +39,6 @@ import {
 import { useToast } from '@/hooks/use-toast';
 import { fetchDashboardData, DashboardData, DashboardFilters } from '@/utils/dashboardApi';
 import ChartGrid from './ChartGrid';
-import { DashboardFilters } from './DashboardFilters';
 
 interface FilterState extends DashboardFilters {
   customFilter: string;
